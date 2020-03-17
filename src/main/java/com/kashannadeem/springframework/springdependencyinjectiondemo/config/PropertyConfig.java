98090d6f08ac5bdb948e0b02df0fcac9911d32ab
Kashan <kashi_nadim@yahoo.com>
2020-03-17 23:12:18 +0200

    Adding support for environment properties

--- src/main/java/com/kashannadeem/springframework/springdependencyinjectiondemo/config/PropertyConfig.java
+++ src/main/java/com/kashannadeem/springframework/springdependencyinjectiondemo/config/PropertyConfig.java
@@ -1,15 +1,21 @@
 package com.kashannadeem.springframework.springdependencyinjectiondemo.config;
 
 import com.kashannadeem.springframework.springdependencyinjectiondemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
     @Value("${kashan.username}")
     String user;
     @Value("${kashan.password}")
@@ -20,7 +26,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
+//        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
